2353e8afa1edac1e45c30ccfaff5237cbfc980d9
zeltak <zeltak@gmail.com>
2014-09-29 17:55:09 +0300

    zuni Mon Sep 29 17:55:09 IDT 2014

--- Uni/Projects/code/P032.multiple_expo_jaime/create_bw_expo_V2.r
+++ Uni/Projects/code/P032.multiple_expo_jaime/create_bw_expo_V2.r
@@ -220,3 +220,36 @@ bwfull.ptv[, c(3,43):= NULL]
 
 saveRDS(bwfull.ptv,"/media/NAS/Uni/Projects/P032_multiple_expo_jaime/3.Work/Archive/tmpsav.rds")
 
+################ LAN
+lanid <- fread("/media/NAS/Uni/Projects/P032_multiple_expo_jaime/3.Work/P007_key_tables/caseid_LANid.csv")
+setnames(lanid, "uniqueid_y","id")
+
+#merge id
+setkey(bwfull.ptv, id)
+setkey(lanid ,id)
+bwfull.ptvl<- merge(bwfull.ptv, lanid[,list(id,geonameID)])
+
+
+
+
+lan00 <-  as.data.table(read.dbf("/media/NAS/Uni/Data/world/LAN/2000.dbf"))
+lan00MA<-filter(lan00, x > -74 , x < -69 , y < 44 , y > 41)
+lan00MA$year<-2000
+write.csv(lan00MA,"/media/NAS/Uni/Projects/P032_multiple_expo_jaime/1.Raw_data/lan_MA/lanMA00.csv")
+
+
+
+lan02 <-  as.data.table(read.dbf("/media/NAS/Uni/Data/world/LAN/2002.dbf"))
+lan02MA<-filter(lan02, x > -74 , x < -69 , y < 44 , y > 41)
+lan02MA$year<-2002
+write.csv(lan02MA,"/media/NAS/Uni/Projects/P032_multiple_expo_jaime/1.Raw_data/lan_MA/lanMA02.csv")
+
+
+
+lan05 <-  as.data.table(read.dbf("/media/NAS/Uni/Data/world/LAN/2005_2006.dbf"))
+lan05MA<-filter(lan05, x > -74 , x < -69 , y < 44 , y > 41)
+lan05MA$year<-2006
+write.csv(lan05MA,"/media/NAS/Uni/Projects/P032_multiple_expo_jaime/1.Raw_data/lan_MA/lanMA06.csv")
+
+
+
