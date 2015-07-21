2d1f2d13bbde4f8248d3eed6a0f1590027ac3933
zeltak <zeltak@gmail.com>
2015-07-21 15:10:43 +0300

    zuni Tue Jul 21 15:10:43 IDT 2015

--- Uni/Projects/code/P031.MAIAC.France/archive/exclude.pm25.miss.stn.r
+++ Uni/Projects/code/P031.MAIAC.France/archive/exclude.pm25.miss.stn.r
@@ -6,6 +6,7 @@ library(reshape)
 library(foreign) 
 library(ggplot2)
 library(plyr)
+library(dplyr)
 library(data.table)
 library(reshape2)
 library(Hmisc)
@@ -14,8 +15,16 @@ library(gdata)
 library(FNN)
 
 
-basestn<-  fread("/media/NAS/Uni/Projects/P031_MAIAC_France/2.work/keys/pm25.stn.XY.csv")
+#x<-  fread("/media/NAS/Uni/Projects/P031_MAIAC_France/2.work/keys/pm25.stn.XY.csv")
+basestn<- fread("/home/zeltak/ZH_tmp/pm25_stn.csv")
 badstn<- fread("/home/zeltak/ZH_tmp/pm25_stn_nodata.csv")
-setnames(basestn, "field_1","stn")
+setnames(basestn, "CODESTATION","stn")
 setnames(badstn, "V1","stn")
 out <- basestn[!(basestn$stn %in% badstn$stn), ] 
+#join XY
+setnames(x, "field_1","stn")
+setkey(out  ,stn)
+setkey(x ,stn )
+pmall <- outer_join(out,x)
+
+write.csv(out,"/home/zeltak/ZH_tmp/Valid_pm25_stn.csv")
